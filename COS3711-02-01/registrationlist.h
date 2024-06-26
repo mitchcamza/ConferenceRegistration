a8bf3a3a544030bff7bd472cfee6b836ffe3b189
mitchcamza <192mitch88@gmail.com>
2024-06-26 15:13:15 -0700

    Implement RegistrationList class

--- COS3711-02-01/registrationlist.h
+++ COS3711-02-01/registrationlist.h
@@ -2,18 +2,60 @@
 #define REGISTRATIONLIST_H
 
 #include "registration.h"
+#include <QObject>
+#include <QList>
 
-class RegistrationList
+
+/**
+ * @class RegistrationList
+ * @brief Manages a list of conference registrations.
+ *
+ * The RegistrationList class provides functionality to add and query conference
+ * registrations. It ensures no duplicate registrations by name and calculates
+ * total fees based on registration types.
+ */
+class RegistrationList : public QObject
 {
+    Q_OBJECT
+
 public:
-    bool addRegistration(Registration* r);
+    /**
+     *  @brief Constructs a RegistrationList.
+     *  @param parent The parent QObject, default is nullptr.
+     */
+    RegistrationList(QObject *parent = nullptr);
+
+    /**
+     * @brief Destructor for RegistrationList.
+     * Cleans up the list of Registrations.
+     */
     ~RegistrationList();
-    bool isRegistered(const QString &n);
-    double totalFee(const QString &t);
-    int totalRegistrations(const QString &a);
+
+    /** @brief Adds attendee to list if their name is not on the list.
+     *  @param r The registration.
+     */
+    bool addRegistration(Registration* r);
+
+    /** @brief Returns true if the attendee (person) is already registered (by name).
+     *  Returns false if the attendee's name is not on the list.
+     *  @note Since a uniqueness check is only performed on names, duplicate Email addresses may exist.
+     *  @param n The attendee's name.
+     */
+    bool isRegistered(const QString &n) const;
+
+    /** @brief Returns the total registration fees for a type of registration
+     *  @note Registration types may include "StandardRegistration", "StudentRegistration", "GuestRegistration", or "All".
+     *  @param t The type of Registration.
+     */
+    double totalFee(const QString &t) const;
+
+    /** @brief Returns the number of attendees that are registered for the conference from an institution.
+     *  @param a The affiliation (institution).
+     */
+    int totalRegistrations(const QString &a) const;
 
 private:
-    QList<Registration*> m_AttendeeList;
+    QList<Registration*> m_AttendeeList; ///< List of registrations.
 };
 
 #endif // REGISTRATIONLIST_H
