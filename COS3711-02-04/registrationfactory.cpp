ecdfc7c90904e38c707cbb235befd0b6bedf96c1
mitchcamza <192mitch88@gmail.com>
2024-07-17 13:13:22 -0700

    Design and implement RegistrationFactory class

---
+++ COS3711-02-04/registrationfactory.cpp
@@ -0,0 +1,38 @@
+#include "registrationfactory.h"
+#include "registration.h"
+#include "standardregistration.h"
+#include "studentregistration.h"
+#include "guestregistration.h"
+#include "person.h"
+
+#include <QDate>
+
+RegistrationFactory &RegistrationFactory::getInstance()
+{
+    static RegistrationFactory registrationFactoryInstance;
+    return registrationFactoryInstance;
+}
+
+Registration *RegistrationFactory::createRegistration(const QString &type, const QString &name, const QString &affiliation, const QString &email, const QDate &bookingDate, const QString &additionalInfo)
+{
+    Person attendee(name, affiliation, email);
+
+    if (type == "standard")
+    {
+        return new StandardRegistration(attendee, bookingDate);
+    }
+    else if (type == "student")
+    {
+        return new StudentRegistration(attendee, bookingDate, additionalInfo);
+    }
+    else if (type == "guest")
+    {
+        return new GuestRegistration(attendee, bookingDate, additionalInfo);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+RegistrationFactory::RegistrationFactory() {}
---
+++ COS3711-02-04/registrationfactory.h
@@ -0,0 +1,24 @@
+#ifndef REGISTRATIONFACTORY_H
+#define REGISTRATIONFACTORY_H
+
+
+class Registration;
+class QString;
+class QDate;
+
+class RegistrationFactory
+{
+public:
+    static RegistrationFactory &getInstance();
+
+    Registration *createRegistration(const QString &type, const QString &name, const QString &affiliation, const QString &email, const QDate &bookingDate, const QString &additionalInfo);
+
+private:
+    RegistrationFactory();
+
+    // Delete copy constructor and assignment operator to prevent copying of RegistrationFactory
+    RegistrationFactory(const RegistrationFactory&) = delete;
+    RegistrationFactory &operator=(const RegistrationFactory&) = delete;
+};
+
+#endif // REGISTRATIONFACTORY_H
